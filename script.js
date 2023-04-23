c5e3f74cdc2bef18c48001d21514041c80fd80f4
bhavana-rm <130067019+bhavana-rm@users.noreply.github.com>
2023-04-23 13:01:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let socials_button = document.getElementById("socials_button");
+let socials = document.getElementById("socials");
+socials.style.display = "none";
+socials_button.addEventListener("click", () => {
+  socials.style.display = "block";
+  console.log("hi");
+
+});
